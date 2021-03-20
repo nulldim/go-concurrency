fcc6ca2e3437ab01d416ec42ac978ba53820be9a
Singh <Pankaj.Singh@broadridge.com>
2021-03-20 21:43:50 +0530

    updated file

--- workerpool/wp.go
+++ workerpool/wp.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	numWorkers := 50
+	c := make(chan string)
+
+	for i := 0; i < numWorkers; i++ {
+		go func(count int) {
+			time.Sleep(time.Millisecond * 5000)
+			c <- fmt.Sprintf("Worker #%d", count)
+		}(i)
+	}
+
+	for res := range <-c {
+		fmt.Println(res)
+	}
 
 }
